474f576cb9e6fb17024a6bd9ee2955b314c84c6e
kaminskimarcin <kaminski283@gmail.com>
2018-01-31 13:50:34 +0100

    Kodilla-17.4

---
+++ showtasks.bat
@@ -0,0 +1,20 @@
+call runcrud.bat
+if "%ERRORLEVEL%" == "0" goto callBrowser
+echo.
+echo Fail with runcrud script! Check it out.
+goto fail
+
+:callBrowser
+start firefox.exe http://localhost:8080/crud/v1/task/getTasks
+if "%ERRORLEVEL%" == "0" goto end
+echo.
+echo Could't open browser.
+goto fail
+
+:fail
+echo.
+echo Breaking script. Fail.
+
+:end
+echo.
+echo Everything goes well.
