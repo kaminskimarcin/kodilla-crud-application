52864899254745cf46656c010747c3dec6d631f0
kaminskimarcin <kaminski283@gmail.com>
2018-02-03 10:25:42 +0100

    18.2

---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,11 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+}
